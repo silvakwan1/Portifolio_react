bd71436ae1b68989ca77f0330dbc0a2e737dbc0c
silvakwan1 <123783319+silvakwan1@users.noreply.github.com>
2023-10-22 02:19:07 -0300

    ;

--- vite.config.js
+++ vite.config.js
@@ -6,8 +6,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // Use caminhos relativos, não caminhos absolutos
-      "@src": "/path-to-your-NavBar-component", // O '@src' é um exemplo, você pode personalizá-lo.
+      "@src": "./src", // Substitua isso pelo caminho real para o diretório onde o arquivo "NavBar" está localizado.
     },
   },
 });
